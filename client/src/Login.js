46364ab256495f9719e8fd70386b31ec51a8abfa
David Dean <vermicious.knid.d@gmail.com>
2024-09-08 15:47:20 -0700

    Adds Axios. Adds http call to login. Sets token in state. Adds error handling and stylingt o login form.

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,14 +1,17 @@
 import { useState } from "react"
+import axios from "axios"
 
 const Login = () => {
   const [ email, setEmail ] = useState("")
   const [ emailNotValid, setEmailNotValid ] = useState(false)
   const [ password, setPassword ] = useState("")
   const [ passwordNotValid, setPasswordNotValid ] = useState(false)
+  const [ isSubmitting, setIsSubmitting ] = useState(false)
+  const [ error, setError ] = useState("")
+  const [ token, setToken ] = useState("")
 
-  // TODO: Get an email regex that actually works
-  // TODO: From https://emailregex.com/ - IE Not from these guys
-  const emailRegexOLDANDBUESTED = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+  // TODO: Add Redux, set token in Redux
+  // TODO: On successful login redirect to dashbaord
 
   // TODO: Make sure username and domain name don't allow multiple consecutive dashes, underscores or periods
   const emailRegex = /^[a-zA-Z0-9\-._]*@[a-zA-Z0-9\-.]*.[.com|.org|.net|.int|.edu|.gov|.mil]$/
@@ -30,8 +33,15 @@ const Login = () => {
   const handleSubmit = (e) => {
     e.preventDefault()
     // Email
-    if (emailRegex.test(email) === false) {
-      // Set form error styles
+    // if (emailRegex.test(email) === false) {
+    //   // Set form error styles
+    //   setEmailNotValid(true)
+    // }
+    // else {
+    //   setEmailNotValid(false)
+    // }
+    // Email logic
+    if (email.length < 4) {
       setEmailNotValid(true)
     }
     else {
@@ -45,9 +55,51 @@ const Login = () => {
     else {
       setPasswordNotValid(false)
     }
-    if (email.length > 5 && password.length >= 4) {
+    if (email.length > 3 && password.length >= 4) {
+      // Disable submit button
+      setIsSubmitting(true)
       // Check login, axios
-      console.log("hunky dory")
+      axios.post(`${process.env.REACT_APP_SERVER}/users/login`,
+        { username: email, password },
+        { headers: { 'Content-Type': 'application/json' }}
+      ).then(res => {
+        console.log(res)
+        // If credentials are not valid
+        if (!res.data.success) {
+          // Check for error code
+          if (res.status === 400) {
+            setError("Please enter all required fields.")
+          }
+          else if (res.status === 401) {
+            setError("Invalid credentials.")
+          }
+          else {
+            setError("There was an error. Please check your credentials and try again.")
+          }
+        }
+        // If creedentials are valid
+        else {
+          const { token } = res.data
+          console.log(token)
+          // Set state
+          setToken(token)
+          // Reset submit button
+          setIsSubmitting(false)
+          // Reset error message
+          setError("")
+          // Redirect to dashboard
+        }
+      }).catch(err => {
+        if (err.status === 401) {
+          // Set error message
+          setError("Invalid credentials.")
+          // Reset submit button
+          setIsSubmitting(false)
+        }
+        else {
+          setError("There was an error. Please check your credentials and try again.")
+        }
+      })
     }
   }
 
@@ -94,10 +146,25 @@ const Login = () => {
         </div>
         <button
           type="submit"
+          disabled={isSubmitting}
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Sign In
         </button>
+        {error && <div id="alert-2" class="flex items-center p-4 mb-4 text-rose-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-rose-400" role="alert">
+  <svg class="flex-shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+  </svg>
+  <span class="sr-only">Info</span>
+  <div class="ms-3 text-sm font-medium">{error}</div>
+  <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-red-50 text-rose-500 rounded-lg focus:ring-2 focus:ring-rose-400 p-1.5 hover:bg-rose-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-rose-400 dark:hover:bg-gray-700" data-dismiss-target="#alert-2" aria-label="Close">
+    <span class="sr-only">Close</span>
+    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+    </svg>
+  </button>
+</div>
+}
       </form>
     </div>
   )
